dc40dc117aaf5109a02488ba3d5fcf5d8d17f9eb
junevig <67835792+junevig@users.noreply.github.com>
2022-07-06 21:15:48 -0500

    correccion

--- app/src/main/java/com/upn/final_app/Fragmento1.java
+++ app/src/main/java/com/upn/final_app/Fragmento1.java
@@ -7,58 +7,64 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragmento1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragmento1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Fragmento1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragmento1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragmento1 newInstance(String param1, String param2) {
-        Fragmento1 fragment = new Fragmento1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText txtNombre, txtCodigo, txtVida, txtEstado;
+    Button btnRegistrar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento1, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragmento1, container, false);
+
+        btnRegistrar = view.findViewById(R.id.btnRegistrar);
+        txtNombre = view.findViewById(R.id.txtNombre);
+        txtCodigo = view.findViewById(R.id.txtCodigo);
+        txtVida = view.findViewById(R.id.txtVida);
+        txtEstado = view.findViewById(R.id.txtEstado);
+        btnRegistrar = view.findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capturarDatos();
+            }
+        });
+
+        return view;
+    }
+
+    private boolean capturarDatos(){
+        String nombre = txtNombre.getText().toString();
+        String codigo = txtCodigo.getText().toString();
+        String vida = txtVida.getText().toString();
+        String estado = txtEstado.getText().toString();
+        boolean valida = true;
+        if (nombre.equals("")){
+            txtNombre.setError("Titulo es obligatorio");
+            valida = false;
+        }
+        if (codigo.equals("")){
+            txtCodigo.setError("Codigo es obligatorio");
+            valida = false;
+        }
+        if (vida.equals("")){
+            txtVida.setError("Tiempo de vida es obligatorio");
+            valida = false;
+        }
+        if (estado.equals("")){
+            txtEstado.setError("Estado de bien es obligatorio");
+            valida = false;
+        }
+        return valida;
     }
 }
--- app/src/main/java/com/upn/final_app/MainActivity.java
+++ app/src/main/java/com/upn/final_app/MainActivity.java
@@ -17,8 +17,6 @@ import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bNavigation;
-    EditText txtNombre, txtCodigo, txtVida, txtEstado;
-    Button btnRegistrar;
 
 
     @Override
@@ -31,19 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void asignarReferencias(){
         bNavigation = findViewById(R.id.bNavigation);
-
-        txtNombre = findViewById(R.id.txtNombre);
-        txtCodigo = findViewById(R.id.txtCodigo);
-        txtVida = findViewById(R.id.txtVida);
-        txtEstado = findViewById(R.id.txtEstado);
-        btnRegistrar = findViewById(R.id.btnRegistrar);
-
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                capturarDatos();
-            }
-        });
 
        bNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -71,28 +56,5 @@ public class MainActivity extends AppCompatActivity {
     private void mostrarFragmento(Fragment fragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
     }
-    private boolean capturarDatos(){
-        String nombre = txtNombre.getText().toString();
-        String codigo = txtCodigo.getText().toString();
-        String vida = txtVida.getText().toString();
-        String estado = txtEstado.getText().toString();
-        boolean valida = true;
-        if (nombre.equals("")){
-            txtNombre.setError("Titulo es obligatorio");
-            valida = false;
-        }
-        if (codigo.equals("")){
-            txtCodigo.setError("Codigo es obligatorio");
-            valida = false;
-        }
-        if (vida.equals("")){
-            txtVida.setError("Tiempo de vida es obligatorio");
-            valida = false;
-        }
-        if (estado.equals("")){
-            txtEstado.setError("Estado de bien es obligatorio");
-            valida = false;
-        }
-        return valida;
-    }
+
 }
